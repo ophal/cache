fed627ef062ad545f9aa8e8d10cbfebf600dd718
Fernando Paredes Garcia <fernando@develcuy.com>
2015-03-11 01:16:47 -0500

    Implements cache module for Ophal.

---
+++ README.md
@@ -0,0 +1,46 @@
+
+### Usage
+
+Example #1:
+
+```Lua
+local cache = ophal.cache
+cache:set('var:1', 'This is data for cache var:1.')
+```
+NOTE: Cache is not an standard module, it registers itself under the global ophal variable.
+
+Example #2:
+
+```Lua
+local cache = ophal.cache
+cache('custom'):set('var:2', {id = 1, somekey = 'storing this Lua table on a different bin'}, time() + 5*60*60)
+```
+NOTE: To specify a different cache bin, pass it as argument of cache. Custom expire time is set as 3rd argument.
+
+
+### Installation
+
+Run the following SQL queries in strict order:
+
+```SQL
+CREATE TABLE cache(id VARCHAR(255) PRIMARY KEY, data TEXT, expire UNSIGNED BIG INT, created UNSIGNED BIG INT, serialized BOOLEAN);
+CREATE INDEX idx_cache_expire ON cache(expire);
+```
+
+If you want to create another cache bin, just change prefix the table name with 'cache_', for example:
+
+```SQL
+CREATE TABLE cache_content(id VARCHAR(255) PRIMARY KEY, data TEXT, expire UNSIGNED BIG INT, created UNSIGNED BIG INT, serialized BOOLEAN);
+CREATE INDEX idx_cache_content_expire ON cache_content(expire);
+```
+
+
+### Configuration
+
+Add the following to settings.lua:
+
+```Lua
+settings.cache = {
+  default_ttl = 1*60*60, -- default: 1 hour
+}
+```
---
+++ init.lua
@@ -0,0 +1,79 @@
+local _M = {bin = 'cache'}
+ophal.cache = _M -- Not an standard module for convenience
+
+local time, table_dump = os.time, seawolf.contrib.table_dump
+local config, xtable = settings.cache, seawolf.contrib.seawolf_table
+
+function _M.get(t, key)
+  local data_function, err, parsed, data
+
+  local rs = db_query('SELECT * FROM ' .. t.bin .. ' WHERE id = ? AND (expire = 0 OR expire >= ?)', key, time())
+  local result = rs:fetch(true)
+
+  if result then
+    if result.serialized then
+      data_function, err = loadstring(result.data)
+      if data_function then
+        setfenv(data_function, {}) -- empty environment
+        parsed, result.data, err = pcall(data_function)
+      end
+      if err then
+        error(format('cache: %s', err))
+      end
+    end
+    return result
+  end
+end
+
+-- Write session data and end session
+function _M.set(t, key, data, expire)
+  local serialized, rawdata, saved, err
+
+  if type(data) == 'table' then
+    rawdata = xtable({'return '})
+    serialized, err = pcall(table_dump, data, function (s) rawdata:append(s) end)
+    if serialized then
+      data = rawdata:concat()
+    else
+      error(format('cache: %s', err))
+    end    
+  else
+    data = tostring(data)
+  end
+
+  db_query('DELETE FROM ' .. t.bin .. ' WHERE id = ?', key)
+  db_query('INSERT INTO ' .. t.bin .. '(id, data, expire, created, serialized) VALUES(?, ?, ?, ?, ?)',
+    key,
+    data,
+    expire or time() + config.default_ttl,
+    time(),
+    serialized
+  )
+
+  return t
+end
+
+local mt = {}
+
+function mt.__call(t, bin)
+  return _M:new(bin)
+end
+
+-- Build a new instance of cache interface
+function _M:new(bin)
+  local m = {}
+
+  for k in pairs(_M) do
+    m[k] = _M[k]
+  end
+
+  m.bin = 'cache_' .. bin
+
+  setmetatable(m, mt)
+
+  return m
+end
+
+setmetatable(_M, mt)
+
+return _M
